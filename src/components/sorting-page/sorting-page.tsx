81f2f78149eaf8d7b5d4ced6106b87a4ea84e07f
Олег Цыпуштанов <madeit@MacBook-Pro-Oleg.local>
2023-09-15 11:32:25 +0300

    Создать компонент для визуализации сортировки

--- src/components/sorting-page/sorting-page.tsx
+++ src/components/sorting-page/sorting-page.tsx
@@ -1,10 +1,49 @@
-import React from "react";
+import styles from "./sorting-page.module.css";
+import React, { ChangeEvent, useEffect, useState } from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { Button } from "../ui/button/button";
+import { RadioInput } from "../ui/radio-input/radio-input";
+import { SortMethods } from "../../types/sorting-types";
+import { Direction } from "../../types/direction";
 
 export const SortingPage: React.FC = () => {
+  const [sortMethod, setSortMethod] = useState<SortMethods>(
+    SortMethods.Selection
+  );
+
   return (
     <SolutionLayout title="Сортировка массива">
-
+      <div className={styles.form}>
+        <RadioInput
+          extraClass="mr-20"
+          label="Выбор"
+          name="sortMethod"
+          checked={sortMethod === SortMethods.Selection}
+          onChange={() => {
+            setSortMethod(SortMethods.Selection);
+          }}
+        />
+        <RadioInput
+          extraClass="mr-25"
+          label="Пузырёк"
+          name="sortMethod"
+          checked={sortMethod === SortMethods.Bubble}
+          onChange={() => {
+            setSortMethod(SortMethods.Bubble);
+          }}
+        />
+        <Button
+          extraClass={`${styles.button_sorting} mr-6`}
+          text="По возрастанию"
+          sorting={Direction.Ascending}
+        />
+        <Button
+          extraClass={`${styles.button_sorting} mr-40`}
+          text="По убыванию"
+          sorting={Direction.Descending}
+        />
+        <Button extraClass={styles.button} text="Новый массив" />
+      </div>
     </SolutionLayout>
   );
 };
