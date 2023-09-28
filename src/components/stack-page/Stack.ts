79406aaeadd39f4b3bc930c47143a820881c8382
Олег Цыпуштанов <madeit@MacBook-Pro-Oleg.local>
2023-09-28 20:46:52 +0300

    feat: Настроить подсветку добавляемых и удаляемых элементов у компонента стека

--- src/components/stack-page/Stack.ts
+++ src/components/stack-page/Stack.ts
@@ -11,21 +11,52 @@ export class Stack<typeItem extends string | number>
   implements IStack<typeItem> {
   _arr: typeItem[] = [];
 
+  /**
+   * Добавление элемента в стек
+   *
+   * @param item - Элемент, который нужно добавить в стек
+   */
   push(item: typeItem) {
     this._arr.push(item);
   }
+
+  /**
+   * Удаление верхнего элемента из стека
+   */
   pop() {
     this._arr.pop();
   }
+
+  /**
+   * Получение верхнего элемента стека
+   *
+   * @returns Верхний элемент стека или undefined, если стек пуст
+   */
   peek(): typeItem | undefined {
     return this._arr[this.size()];
   }
+
+  /**
+   * Очищение всех элементов из стека
+   */
   clear() {
     this._arr = [];
   }
+
+  /**
+   * Возвращает массив стека
+   *
+   * @returns Массив содержащий элементы стека
+   */
   toArray() {
     return [...this._arr];
   }
+
+  /**
+   * Возвращает количество элементов в стеке
+   *
+   * @returns Количество элементов в стеке
+   */
   size() {
     return this._arr.length - 1;
   }
--- src/components/stack-page/stack-page.tsx
+++ src/components/stack-page/stack-page.tsx
@@ -1,41 +1,81 @@
-import React, { ChangeEvent, useEffect, useState } from "react";
+import styles from "./stack-page.module.css";
+import React, { ChangeEvent, useState } from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 import { Button } from "../ui/button/button";
 import { Input } from "../ui/input/input";
-import styles from "./stack-page.module.css";
 import { Circle } from "../ui/circle/circle";
 import { Stack } from "./Stack";
 import { delay } from "../utils/utils";
 import { ElementStates } from "../../types/element-states";
 
+//Инстанс класса имитирующий структуру стека
 const stack = new Stack<string>();
 
 export const StackPage: React.FC = () => {
   const [stackState, setStackState] = useState<string[]>([]);
   const [inputValue, setInputValue] = useState<string>("");
+  const [circleState, setCircleState] = useState<ElementStates[]>([]);
 
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
     const targetValue = e.target.value;
     setInputValue(targetValue);
   };
 
-  const handleAddItem = () => {
+  /**
+   *Функция добавления последнего элемента в стек
+   *
+   */
+  const handleAddItem = async () => {
     stack.push(inputValue);
     const item = stack.peek();
-    item && setStackState((prev) => [...prev, item]);
-    setInputValue("");
+
+    if (item) {
+      setCircleState((prev) => [...prev, ElementStates.Changing]);
+      setStackState((prev) => [...prev, item]);
+      await delay(500);
+      updateCircleColor(ElementStates.Default);
+      setInputValue("");
+    }
   };
 
+  /**
+   *Функция удаления последнего элемента из стека
+   *
+   */
   const handleDeleteItem = async () => {
+    updateCircleColor(ElementStates.Changing);
+    await delay(500);
     stack.pop();
     setStackState(stack.toArray());
+
+    setCircleState((prev) => {
+      const newArray = [...prev];
+      newArray.pop();
+      return newArray;
+    });
   };
 
+  /**
+   *Функция обнуления массива
+   *
+   */
   const handleClearArray = () => {
     stack.clear();
     setStackState(stack.toArray());
+    setCircleState([]);
   };
 
+  /**
+   *Функция  обновляющая цвет у элемента
+   *
+   * @param {ElementStates} elementStates цвет элемента (выбирается из коллекции)
+   */
+  const updateCircleColor = (elementStates: ElementStates) => {
+    setCircleState((prev) => {
+      prev[prev.length - 1] = elementStates;
+      return [...prev];
+    });
+  };
 
   return (
     <SolutionLayout title="Стек">
@@ -68,12 +108,12 @@ export const StackPage: React.FC = () => {
         {stackState.map((item, index, arr) => {
           return (
             <Circle
-              state = {ElementStates.Modified}
+              extraClass={styles.circle}
+              state={circleState[index]}
               head={arr.length - 1 === index ? "top" : ""}
               letter={`${item}`}
               key={index}
               index={index}
-              extraClass={styles.circle}
             />
           );
         })}
