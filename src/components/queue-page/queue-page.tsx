e2b9963a636aebffdf6d8ae98c87a991ce809365
Олег Цыпуштанов <madeit@MacBook-Pro-Oleg.local>
2023-10-01 10:33:14 +0300

    feat: Добавить в компонент очереди изначальный массив с пустыми значениями

--- src/components/queue-page/queue-page.tsx
+++ src/components/queue-page/queue-page.tsx
@@ -1,12 +1,25 @@
 import styles from "../../styles/data-structure.module.css";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 import { Input } from "../ui/input/input";
 import { Button } from "../ui/button/button";
 import useInput from "../../hooks/useInput";
+import { ElementStates } from "../../types/element-states";
+import { Circle } from "../ui/circle/circle";
 
 export const QueuePage: React.FC = () => {
   const [inputValue, setInputValue, handleInputChange] = useInput("");
+  const [queueState, setQueueState] = useState<string[]>([]);
+  const [circleState, setCircleState] = useState<ElementStates[]>([]);
+
+  useEffect(() => {
+    const arr = [];
+    for (let i = 0; i < 7; i++) {
+      arr.push("");
+    }
+
+    setQueueState(arr);
+  }, []);
 
   return (
     <SolutionLayout title="Очередь">
@@ -22,7 +35,20 @@ export const QueuePage: React.FC = () => {
         <Button text="Удалить" />
         <Button extraClass={styles.button} text="Очистить" />
       </div>
-      <div className={styles.container}></div>
+      <div className={styles.container}>
+        {queueState.map((item, index, arr) => {
+          return (
+            <Circle
+              extraClass={styles.circle}
+              state={circleState[index]}
+              head={item ? "top" : ""}
+              letter={`${item}`}
+              key={index}
+              index={index}
+            />
+          );
+        })}
+      </div>
     </SolutionLayout>
   );
 };
