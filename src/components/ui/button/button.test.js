2f3c159eb644558025440f9e3027daff5d52f7f0
Олег Цыпуштанов <madeit@MacBook-Pro-Oleg.local>
2023-10-23 23:09:21 +0300

    feat: cгруппировать тесты для кнопки в один блок

--- src/components/ui/button/button.test.js
+++ src/components/ui/button/button.test.js
@@ -3,33 +3,36 @@ import renderer from "react-test-renderer";
 import React from "react";
 import { Button } from "./button";
 
-it("отрисовка с текстом", () => {
-  const tree = renderer.create(<Button text="какой-то текст" />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
+describe("Button", () => {
+  it("отрисовка с текстом", () => {
+    const tree = renderer.create(<Button text="какой-то текст" />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 
-it("отрисовка без текста", () => {
-  const tree = renderer.create(<Button />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
+  it("отрисовка без текста", () => {
+    const tree = renderer.create(<Button />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 
-it("отрисовка заблокированной кнопки", () => {
-  const tree = renderer.create(<Button disabled={true} />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
+  it("отрисовка заблокированной кнопки", () => {
+    const tree = renderer.create(<Button disabled={true} />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 
-it("отрисовка кнопки с индикацией загрузки", () => {
-  const tree = renderer.create(<Button isLoader={true} />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
+  it("отрисовка кнопки с индикацией загрузки", () => {
+    const tree = renderer.create(<Button isLoader={true} />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 
-// Проверяем корректность вызова колбека при клике на кнопку.
+  // Проверяем корректность вызова колбека при клике на кнопку.
 
-it("Нажатие на кнопку вызывает колбэк корректно", () => {
-  const onClickMock = jest.fn();
+  it("Нажатие на кнопку вызывает колбэк корректно", () => {
+    const onClickMock = jest.fn();
 
-  const tree = render(<Button onClick={onClickMock} text="кнопка" />);
-  const button = tree.getByText("кнопка");
-  fireEvent.click(button);
-  expect(onClickMock).toHaveBeenCalled();
+    const tree = render(<Button onClick={onClickMock} text="кнопка" />);
+    const button = tree.getByText("кнопка");
+    fireEvent.click(button);
+    expect(onClickMock).toHaveBeenCalled();
+    expect(onClickMock).toHaveBeenCalledTimes(1);
+  });
 });
