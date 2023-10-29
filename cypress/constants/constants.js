06235523b3e72189aae9b3bc2287ed94bb290ed1
Олег Цыпуштанов <madeit@MacBook-Pro-Oleg.local>
2023-10-29 15:18:42 +0300

    feat: Написать е2е тест для страницы разворота строки

---
+++ cypress/constants/constants.js
@@ -0,0 +1,5 @@
+export const borderColorShape = {
+  DEFAULT: "rgb(0, 50, 255)",
+  CHANGING: "rgb(210, 82, 225)",
+  MODIFIED: "rgb(127, 224, 81)",
+};
---
+++ cypress/e2e/sting/string.cy.js
@@ -0,0 +1,96 @@
+import { borderColorShape } from "../../constants/constants";
+
+describe("Тесты для страницы рекурсии", function () {
+  beforeEach(() => {
+    cy.visit("/recursion");
+    cy.contains("Строка");
+
+    cy.get("input[type='text']").as("input");
+    cy.get('[data-testid="btnReverseStr"]').as("BtnReverseStr");
+  });
+
+  it("должен проверить, что если в инпуте пусто, то кнопка разворота недоступна", () => {
+    cy.get("@input").should("have.value", "");
+    cy.get("@BtnReverseStr").should("be.disabled");
+  });
+
+  it("должен проверить, что если в инпуте есть текст, то кнопка разворота доступна", () => {
+    cy.get("@input").type("12345");
+    cy.get("@BtnReverseStr").should("not.be.disabled");
+  });
+
+  it("должен корректно развернуть строку и отследить анимацию подсветки изменяемых элементов, ", () => {
+    cy.get("@input").type("12345");
+    cy.get("@BtnReverseStr").click();
+
+    cy.get("[data-testid='circle']").should("have.length", 5).as("circle");
+
+    cy.get("@circle").each(($el, index) => {
+      cy.get($el).contains("12345"[index]);
+      if (index === 0 || index === 4) {
+        cy.get($el).should(
+          "have.css",
+          "border-color",
+          borderColorShape.CHANGING
+        );
+      } else {
+        cy.get($el).should(
+          "have.css",
+          "border-color",
+          borderColorShape.DEFAULT
+        );
+      }
+    });
+
+    cy.get("@circle").each(($el, index) => {
+      cy.get($el).contains("52341"[index]);
+      switch (index) {
+        case 0:
+        case 4:
+          cy.get($el).should(
+            "have.css",
+            "border-color",
+            borderColorShape.MODIFIED
+          );
+          break;
+        case 1:
+        case 3:
+          cy.get($el).should(
+            "have.css",
+            "border-color",
+            borderColorShape.CHANGING
+          );
+          break;
+        default:
+          cy.get($el).should(
+            "have.css",
+            "border-color",
+            borderColorShape.DEFAULT
+          );
+          break;
+      }
+    });
+
+    cy.get("@circle").each(($el, index) => {
+      cy.get($el).contains("54321"[index]);
+      if (index <= 1 || index >= 3) {
+        cy.get($el).should(
+          "have.css",
+          "border-color",
+          borderColorShape.MODIFIED
+        );
+      } else {
+        cy.get($el).should(
+          "have.css",
+          "border-color",
+          borderColorShape.CHANGING
+        );
+      }
+    });
+
+    cy.get("@circle").each(($el, index) => {
+      cy.get($el).contains("54321"[index]);
+      cy.get($el).should("have.css", "border-color", borderColorShape.MODIFIED);
+    });
+  });
+});
