b334f062e4a113a2c7863543d968323ce47c4483
Олег Цыпуштанов <madeit@MacBook-Pro-Oleg.local>
2023-11-05 15:15:59 +0300

    feat: Создать кастомную команду по поиску атрибутов в cypress

--- cypress/constants/constants.js
+++ cypress/constants/constants.js
@@ -3,3 +3,18 @@ export const borderColorShape = {
   CHANGING: "rgb(210, 82, 225)",
   MODIFIED: "rgb(127, 224, 81)",
 };
+
+export const dataSelectors = {
+  BTN_REVERSE_STRING: "btnReverseStr",
+  BTN_GET_FIBONACCI: "btnGetFibonacci",
+  BTN_ADD_ITEM: "btnAddItem",
+  BTN_DELETE_ITEM: "btnDeleteItem",
+  BTN_CLEAR_ALL: "btnClearAll",
+  CIRCLE: "circle",
+  BTN_ADD_ITEM_TO_HEAD: "btnAddItemToHead",
+  BTN_ADD_ITEM_TO_TAIL: "btnAddItemToTail",
+  BTN_ADD_ITEM_TO_INDEX: "btnAddItemToIndex",
+  BTN_DELETE_ITEM_TO_HEAD: "btnDeleteItemToHead",
+  BTN_DELETE_ITEM_TO_TAIL: "btnDeleteItemToTail",
+  BTN_DELETE_ITEM_TO_INDEX: "btnDeleteItemToIndex",
+};
